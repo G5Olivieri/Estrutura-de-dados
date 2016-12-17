479773d83406bb55573b64e49d8065de4100a51e
G5Olivieri <glayson.murollo75@gmail.com>
2016-12-17 18:00:17 -0200

    Update main.c

--- Arvore-Binaria/main.c
+++ Arvore-Binaria/main.c
@@ -1,152 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct arvore
 {
-    int valor;
-    int tamanho;
-    struct arvore* direita;
-    struct arvore* esquerda;
+	int valor;
+	struct arvore* direita;
+	struct arvore* esquerda;
 }no, *pno;
 
-void inicia(pno a);
-int insereRoot(pno a, int valor);
-int inserir(pno a, int valor);
-void percorrer(pno a);
-void percorrerPreOrdem(pno a);
-void remover(pno a, int valor);
-pno minimo(pno a);
+pno inserir(int num)
+{
+	pno ptr = (pno)malloc(sizeof(no));
+	ptr->valor = num;
+	ptr->direita = NULL;
+	ptr->esquerda = NULL;
+	return ptr;
+}
+
+void insere(pno a, int num)
+{
+	if(a)
+	{
+		if(num < a->valor)
+		{
+			if(!a->esquerda)
+				a->esquerda = inserir(num);
+			
+			else
+				insere(a->esquerda, num);
+		}
+		
+		else if(num > a-> valor)
+		{
+			if(!a->direita)
+				a->direita = inserir(num);
+			else
+				insere(a->direita, num);
+		}
+		
+		//else
+		//	printf("Elemento ja existe");	
+	}
+}
+
+void imprime(pno a)
+{
+	if(a)
+	{
+		imprime(a->esquerda);
+		printf("%d ", a->valor);		
+		imprime(a->direita);
+	}
+}
+
+void limpa(pno a, pno ant)
+{
+	if(a)
+	{
+		limpa(a->esquerda, a);		
+		if((!a->direita) && (!a->esquerda))
+			if(a->valor < ant->valor)
+			{
+				ant->esquerda = NULL;
+				free(a);
+				limpa(ant->direita, ant);
+			}
+			else if(a->valor > ant->valor)
+			{
+				ant->direita = NULL;
+				free(a);
+			}
+		limpa(a->direita, a);
+	}
+}
 
 int main()
 {
-    return 0;
+	pno root = inserir(10);
+	long int i;
+	time_t inicia = time(NULL);
+	for(i = 0; i < 1000001; i++) insere(root, rand()%1000001);
+	printf("%d\n", time(NULL) - inicia);
+	scanf("%d", &inicia);
+	inicia = time(NULL);	
+	imprime(root);
+	printf("\n%d\n", time(NULL) - inicia);
+	scanf("%d", &inicia);
+	limpa(root, root);
+	printf("\n\n");
+	free(root);
+	return 0;
 }
-
-void inicia(pno a)
-{
-    a->direita = a->esquerda = NULL;
-    a->tamanho = 0;
-}
-
-int insereRoot(pno a, int valor)
-{
-    pno ptr = (pno)malloc(sizeof(no));
-    if(ptr) return -1;
-    else
-    {
-        a->valor = valor;
-        a->direita = NULL;
-        a->esquerda = NULL;
-        a->tamanho++;
-        return 0;
-    }
-}
-
-int inserir(pno a, int valor)
-{
-    pno ptr = (pno)malloc(sizeof(no));
-    pno atual = a;
-    if(ptr) return -1;
-    else
-    {
-        ptr->valor = valor;
-        if(ptr->valor > atual->valor)
-        {
-            while(atual->valor < ptr->valor) && (!atual->direita)
-            {
-                atual = atual->direita;
-            }
-            atual->direita = ptr;
-            atual->direita->direita = NULL;
-            atual->direita->esquerda = NULL;
-            
-            if(atual->valor > ptr->valor)
-                atual->esquerda = ptr;
-        }
-        else
-        {
-            while(atual->valor > ptr->valor) && (!atual->esquerda)
-            {
-                atual = atual->esquerda;
-            }
-            atual->esquerda = ptr;
-            atual->esquerda->direita = NULL;
-            atual->esquerda->esquerda = NULL;
-            
-            if(atual->valor < ptr->valor)
-                atual->direita = ptr;
-        }
-        a->tamanho++;
-        return 0;
-    }
-}
-
-void percorrer(pno a)
-{
-    if(a)
-    {
-        percorrer(a->esquerda);
-        printf("%d ", a->valor);
-        percorrer(a->direita);
-    }
-}
-
-void percorrerPreOrdem(pno a)
-{
-    if(a)
-    {
-        printf("%d ", a->valor);
-        percorrer(a->esquerda);
-        percorrer(a->direita);
-    }
-}
-
-void remover(pno a, int valor)
-{
-    pno aux;
-    if(a)
-    {
-        if(valor > a->valor)
-        {
-            remover(a->direita, valor);
-        }
-        else
-        {
-            if(valor < a->valor)
-            {
-                remover(a->esquerda, valor);
-            }
-            else
-            {
-                if((a->esquerda) && (a->direita))
-                {
-                    aux = minimo((*a)->direita);
-                    a->valor = aux->valor;
-                    remover(a->direita, a->valor);
-                }
-                else
-                {
-                    aux = *a;
-                    if(!a->esquerda)
-                    {
-                        a = a->direita;
-                    }
-                    else
-                    {
-                        *a->direita = *a->esquerda;
-                    }
-                    free(aux);
-                    a->tamanho--;
-                }
-            }
-        }
-    }
-}
-
-pno minimo(pno a)
-{
-    if(!a) return NULL;
-    else if(!a->esquerda) return a;
-    else minimo(a->esquerda);
-}
-
