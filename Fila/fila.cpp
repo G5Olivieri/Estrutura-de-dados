331ee0282ee469631250e5e127da54c5df265b87
Glayson Olivieri <glayson.murollo75@gmail.com>
2017-05-25 11:32:42 -0300

    Update fila.cpp

--- Fila/fila.cpp
+++ Fila/fila.cpp
@@ -1,146 +1,49 @@
-#include "fila.h"
-
-fila::fila()
+#include "Fila.h"
+#include <cstddef>
+#include <iostream>
+bool Fila::vazia()
 {
-	this->value = 0;
-	this->size = 0;
-	this->next = NULL;
+    return getInicio() == NULL;
 }
 
-void fila::setValue(int n)
+void Fila::push(int valor)
 {
-	this->value = n;
+    if(vazia())
+    {
+        setInicio(new No(valor));
+        setFim(getInicio());
+    }
+    else
+    {
+        this->getFim()->setProx(new No(valor));
+        setFim(this->getFim()->getProx());
+    }
 }
 
-int fila::getValue()
+int Fila::pop()
 {
-	return this->value;
+    int valor = getInicio()->getValor();
+    if(vazia())
+    {
+        return VAZIA;
+    }
+    else
+    {
+        No *temp = getInicio();
+        setInicio(getInicio()->getProx());
+        delete temp;
+    }
+    return valor;
+
 }
 
-void fila::setSize(int n)
+std::ostream& operator<<(std::ostream& out, Fila *f)
 {
-	this->size = n;
-}
-
-int fila::getSize()
-{
-	return this->size;
-}
-
-void fila::setNext(fila* f)
-{
-	this->next = f;
-}
-
-fila* fila::getNext()
-{
-	return this->next;
-}
-
-bool fila::testEmpty(fila* f)
-{
-	if(!(f->getNext())) return true;
-	else return false;
-}
-
-fila* fila::pusher()
-{
-	fila* ptr = new fila();
-	int n;
-	if(!ptr)
-	{
-		std::cout << "Sem memoria: ";
-		return NULL;
-	}
-	else
-	{
-		std::cout << "Digite o valor: ";
-		std::cin >> n;
-		ptr->setValue(n);
-		ptr->setNext(NULL);
-		return ptr;
-	}
-}
-
-fila* fila::lastElement(fila* f)
-{
-	if(!f->getNext()) return f;
-	else return lastElement(f->getNext());
-}
-
-void fila::push(fila* f)
-{
-	fila* ptr = pusher();
-	if(testEmpty(f))
-	{
-		f->setNext(ptr);
-		setSize(getSize()+1);
-	}
-	else
-	{
-		fila* temp = lastElement(f);
-		temp->setNext(ptr);
-		setSize(getSize()+1);
-	}
-}
-
-void fila::pop(fila* f)
-{
-	fila* ptr = f->getNext();
-	if(testEmpty(f)) std::cout << "Fila vazia: ";
-	else
-	{
-		f->setNext(f->getNext()->getNext());
-		std::cout << "Elemento removido: " << ptr->getValue() << std::endl;
-		delete ptr;
-	}
-}
-
-void fila::free(fila* f)
-{
-	if(testEmpty(f)) std::cout << "Fila vazia: ";
-	else
-	{
-		fila* current = f->getNext();
-		fila* next;
-		while(current)
-		{
-			next = current->getNext();
-			delete current;
-			current = next;
-		}
-		std::cout << "Fila vazia: ";
-	}
-}
-
-fila* fila::show(fila* f)
-{
-	if(!(f->getNext())) return NULL;
-	else
-	{
-		std::cout << f->getNext()->getValue() << std::endl;
-		return show(f->getNext());
-	}
-}
-
-void fila::order(fila* f)
-{
-	int temp;
-	fila* ptr = f->getNext();
-	fila* cpr;
-	while(ptr)
-	{
-		cpr = ptr->getNext();
-		for(int i = 0; cpr; i++)
-		{
-			if(ptr->getValue() > cpr->getValue())
-			{
-				temp = ptr->getValue();
-				ptr->setValue(cpr->getValue());
-				cpr->setValue(temp);
-			}
-			cpr = cpr->getNext();
-		}
-		ptr = ptr->getNext();
-	}
+    No *temp = f->getInicio();
+    while(temp)
+    {
+        out << temp->getValor() << " ";
+        temp = temp->getProx();
+    }
+    return out;
 }
